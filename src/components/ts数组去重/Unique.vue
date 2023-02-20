0b4d37c4efa9ef263278c7c7e697f0696635a0f4
LY <15677355462@163.com>
2023-02-20 23:58:57 +0800

    ts对象数组去重

--- src/components/ts数组去重/Unique.vue
+++ src/components/ts数组去重/Unique.vue
@@ -39,9 +39,9 @@ let demo: Demo[] = [
  * 对象数组完全去重，对象一模一样去重
  * @param arr 去重的对象数组
  */
-function unique1(arr: any[]) {
+function unique1<T>(arr: T[]): T[] {
   let map = new Map<string, boolean>();
-  let result = new Set();
+  let result = new Set<T>();
 
   for (let i = 0; i < arr.length; i++) {
     let k = JSON.stringify(arr[i]);
@@ -59,7 +59,7 @@ function unique1(arr: any[]) {
  * 对象数组，根据某属性去重，使用filter() 和 findIndex() 方法搭配
  * @param arr 去重的对象数组
  */
-function unique2<T>(arr: Array<T>, key: keyof T) {
+function unique2<T>(arr: Array<T>, key: keyof T): T[] {
   return arr.filter((item: T, index: number) => {
     return index === arr.findIndex(t => t[key] === item[key])
   })
@@ -69,10 +69,11 @@ function unique2<T>(arr: Array<T>, key: keyof T) {
  * 对象数组，根据某属性去重，使用reduce() 方法
  * @param arr 去重的对象数组
  */
-function unique3(arr: any[], uniId: string): any[] {
+function unique3<T>(arr: T[], key: keyof T): T[] {
   let hash: { [key: string]: boolean } = {};
-  return arr.reduce((accum: any[], item: any) => {
-    hash[item[uniId]] ? "" : (hash[item[uniId]] = true, accum.push(item));
+
+  return arr.reduce((accum: T[], item: T) => {
+    hash[item[key] as string] ? "" : (hash[item[key] as string] = true, accum.push(item));
     return accum;
   }, []);
 }
@@ -82,7 +83,7 @@ function unique3(arr: any[], uniId: string): any[] {
  * 使用map.values() 方法返回一个新的迭代器对象。它包含按顺序插入 Map 对象中每个元素的 value 值
  * @param arr 去重的对象数组
  */
-function unique4(arr: any[], key: string): any[] {
+function unique4<T>(arr: T[], key: keyof T): T[] {
   let map = new Map();
   for (let i of arr) {
     if (!map.has(i[key])) {
