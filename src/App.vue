760f4651a019aefef088966a0ca1a4360f7019d8
LY <15677355462@163.com>
2023-02-28 18:44:51 +0800

    添加ts节流

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,17 @@
 <template>
   <el-config-provider :locale="zhCn">
+    <el-button @click="onClick($event)">点击</el-button>
   </el-config-provider>
 </template>
 
 <script setup lang="ts">
 import { zhCn } from 'element-plus/lib/locale/index'
-import { clone, deepCopy } from "./components/tools/2_深拷贝/CloneDeep";
+import { throttle } from './components/tools/3_节流和防抖/throttle';
+// import { throttle } from 'lodash';
 
-const map = new Map();
-map.set('key', 'value');
-map.set('ConardLi', 'code秘密花园');
-
-const set = new Set();
-set.add('ConardLi');
-set.add('code秘密花园');
-
-const target = {
-  field1: 1,
-  field2: undefined,
-  field3: {
-    child: 'child'
-  },
-  field4: [2, 4, 8],
-  empty: null,
-  map,
-  set,
-  bool: new Boolean(true),
-  num: new Number(2),
-  str: new String(2),
-  symbol: Object(Symbol(1)),
-  date: new Date(),
-  reg: /\d+/,
-  error: new Error('zheshiyige'),
-  func1: () => {
-    console.log('code秘密花园');
-  },
-  func2: function (a: number, b: number) {
-    return a + b;
-  }
-}
-
-let demo = {
-  func2: function (a: number, b: number) {
-    return a + b;
-  }
-}
-
-let newDemo = deepCopy(demo)
-console.log('深拷贝=====>', demo === newDemo);
-
-console.log(demo);
-demo.func2 = function (a: number, b: number) {
-  return a * b;
-}
-
-console.log(newDemo);
-
-
-
-
+let onClick = throttle((a: Event) => {
+  console.log('节流后执行');
+}, 3000, true)
 
 </script>
 
---
+++ src/components/tools/3_节流和防抖/throttle.ts
@@ -0,0 +1,49 @@
+/**
+ * 通过时间差实现
+ * @param handle 要节流的函数
+ * @param wait 需要节流的毫秒
+ * @param immediate 是否立刻执行
+ * @returns 
+ */
+function throttle(handle: Function, wait?: (number | boolean), immediate?: boolean) {
+  if (typeof handle !== 'function') throw new Error('handle must be an function')
+  if (typeof wait === 'undefined') wait = 1000
+  //如果只传入 handle 和 immediate
+  if (typeof wait === 'boolean') {
+    immediate = wait
+    wait = 1000
+  }
+  //immediate默认为flase
+  if (typeof immediate !== 'boolean') immediate = false
+  //定义变量记录上一次执行的时间
+  let previous: number = 0
+  let timer: ReturnType<typeof setTimeout> | undefined = undefined
+  return function (this: any, ...args: any[]) {
+    //获取当前时间
+    let now = +new Date()
+    //如果不立即执行，则interval一直等于wait时间
+    if (!immediate) previous = now
+    // 计算间隔时间
+    let interval = (wait as number) - (now - previous)
+    if (interval <= 0) {
+      // 此刻就说明是一个非高频次操作,可以执行操作
+      clearTimeout(timer)
+      timer = undefined
+      handle.apply(this, args)
+      previous = +new Date()
+
+    } else if (!timer && !immediate) {
+      //当我们发现系统中有一个定时器了，就意味着我们不需要再开定时器
+      //此刻就说明这次的操作发生了在我们定义的频次时间范围内，那就不应该执行handle
+      // 这个时候我们就可以自定义一个定时器，让handle在interval之后去执行
+      timer = setTimeout(() => {
+        clearTimeout(timer) //清除定时器
+        timer = undefined
+        handle.apply(this, args)
+        previous = +new Date()
+      }, interval)
+    }
+  }
+}
+
+export { throttle }
