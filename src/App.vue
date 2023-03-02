a2804be4151711ae3007dc496f0eae497007b5e3
LY <15677355462@163.com>
2023-03-02 10:19:46 +0800

    添加必要注释

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <el-config-provider :locale="zhCn">
-    <el-button @click="onClick($event)">点击</el-button>
+    <el-button>点击</el-button>
   </el-config-provider>
 </template>
 
 <script setup lang="ts">
 import { zhCn } from 'element-plus/lib/locale/index'
-import { throttle } from './components/tools/3_节流和防抖/throttle';
+import { debounce } from './components/tools/3_节流和防抖/debounce';
 // import { throttle } from 'lodash';
 
-let onClick = throttle((a: Event) => {
-  console.log('节流后执行');
-}, 3000, true)
-
+let a = '11'
+let demo = {
+  a: 'a',
+  b: function () {
+    console.log(this.a);
+  },
+  c: () => {
+    console.log(this);
+    // console.log(this.a);
+  }
+}
+demo.b()
+demo.c()
 </script>
 
 <style scoped></style>
