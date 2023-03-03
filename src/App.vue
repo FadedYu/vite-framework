86dd3564f6ac1ea321c76a9f429e3de9bd50532f
LY <15677355462@163.com>
2023-03-03 19:03:06 +0800

    测试代码规范

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <el-config-provider :locale="zhCn">
-    <el-button>点击</el-button>
+    <el-button>{{ x.toFixed(2) }}</el-button>
   </el-config-provider>
 </template>
 
 <script setup lang="ts">
 import { zhCn } from 'element-plus/lib/locale/index'
 
+let x = 1
 </script>
 
 <style scoped></style>
