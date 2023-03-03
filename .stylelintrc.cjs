fb6a9c61e0d576bd249a5bb88f1c345444cd5155
LY <15677355462@163.com>
2023-03-03 19:02:49 +0800

    修改stylelint规则

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -13,6 +13,8 @@ module.exports = {
   rules: {
     // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
     'no-descending-specificity': null,
+    // 允许文件空数据，主要在vue文件中，允许style标签可以为空
+    'no-empty-source': null,
     'selector-pseudo-element-no-unknown': [
       true,
       {
