0d3278ca09fe6897ca848c408561b9f9233e8a03
LY <15677355462@163.com>
2023-03-14 18:47:18 +0800

    添加按需引入插件，移除全局elementplus引入，优化分包策略

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-// elementUI
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
 // 自定义插件 loading
 import customLoading from './components/plugins/Loading'
 
 const app = createApp(App)
-app.use(ElementPlus)
 app.use(customLoading)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunk } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 打包文件大小可视化视图预览插件，查看打包模块占用的空间
+import { visualizer } from 'rollup-plugin-visualizer'
+
+// 自动import与按需导入策略插件
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+// elementPlus 按需导入插件
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+const getVendorChunk = splitVendorChunk()
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
+    visualizer({
+      emitFile: true,
+      filename: 'stats.html'
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        /**
+         * 2.以函数的形式使用
+         * 将第三方包全部打包在一个chunk中，名称叫 vendor
+         */
+        manualChunks: (id, options) => {
+          if (id.indexOf('lodash') !== -1) {
+            return 'lodash'
+          }
+          return getVendorChunk(id, options)
+        }
+      }
+    }
+  }
 })
