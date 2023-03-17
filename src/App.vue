60fe2d16ea1c517d6e4769a12a297f1d1a6eaac0
LY <15677355462@163.com>
2023-03-17 18:02:35 +0800

    添加test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <el-config-provider :locale="zhCn">
-    <div>hello world</div>
+    <div>{{ hello }}</div>
+    <custom-date-picker></custom-date-picker>
   </el-config-provider>
 </template>
 
 <script setup lang="ts">
-import { zhCn } from 'element-plus/lib/locale/index'
+import { zhCn } from 'element-plus/es/locale/index'
+import { ref } from 'vue'
+
+import CustomDatePicker from '@/components/views/CustomDatePicker.vue'
+
 import './test/test'
+let hello = ref('hello world')
 </script>
 
 <style scoped></style>
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,1 +1,8 @@
+const a = 'hello'
+const b = a.substring(1)
+console.log(b)
+
+const c = b.split('/')
+console.log(c)
+
 export {}
