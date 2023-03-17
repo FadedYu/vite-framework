406b61365c19d2dba6ef3888870009288c53ac65
LY <15677355462@163.com>
2023-03-17 18:02:53 +0800

    添加router

---
+++ src/router/systemRouter.ts
@@ -0,0 +1,11 @@
+const routes = [
+  {
+    path: '/SnakeGame',
+    component: () => import('@/components/module/2_贪吃蛇游戏/index.vue'),
+    meta: {
+      title: '找回密码'
+    }
+  }
+]
+
+export default { routes }
